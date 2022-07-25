671b5a8a468b57650e6ee9a8fe8e23fd3b914583
Akhil Satish <akhilsatish2008@gmail.com>
2022-07-25 16:40:05 -0700

    created update method and delete method for tasks

--- server.js
+++ server.js
@@ -47,28 +47,37 @@ app.post('/', async (req, res) => {
 //Update Method
 app
     .route("/edit/:id")
-    .get((req,res)=>{
-        const id = req.params.id
-        TodoTask.find({}, (err,tasks)=>{
-            res.render('edit.ejs',{
-                todoTasks:tasks, idTask:id
-            })
-        })
-    .post((req,res)=>{
-        const id = req.params.id
+    .get((req, res) => {
+        const id = req.params.id;
+        TodoTask.find({}, (err, tasks) => {
+            res.render("edit.ejs", { todoTasks: tasks, idTask: id });
+        });
+    })
+    .post((req, res) => {
+        const id = req.params.id;
         TodoTask.findByIdAndUpdate(
             id,
             {
-                title:req.body.title,
+                title: req.body.title,
                 content: req.body.content
             },
+
             err => {
-                if (err) return res.status(500).send(err)
-                res.redirect("/")
-            }
-        )
-    })    
-})
+                if (err) return res.status(500).send(err);
+                res.redirect("/");
+            });
+    });
+//dELETE 
+app
+    .route("/remove/:id") 
+    .get((req,res)=>{
+        const id = req.params.id
+        TodoTask.findByIdAndRemove(id, err => {
+            if (err) return res.status(500).send(err);
+            res.redirect('/')
+        })
+    })   
+
 
 
 //Start Server
