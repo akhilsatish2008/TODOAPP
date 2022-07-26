02aaa4095271f811082e212bbd0ed826b04615f8
Akhil Satish <akhilsatish2008@gmail.com>
2022-07-25 18:23:36 -0700

    deployment-testing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Declare Variables
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const TodoTask = require("./models/todotask");
 require('dotenv').config()
